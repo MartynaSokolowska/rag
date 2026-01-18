259f0d0dd85ed36eb5473ac401f8c10854e282cb
marti3 <marcioch.s.m@gmail.com>
2026-01-18 21:26:07 +0100

    ES & Qdrant initialization

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+
+from retrieval.elastic import initialize_elsticsearch
+from retrieval.qdrant import load_data_to_qdrant
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 import rag_query
 from utils.data_preparation import load_data_from_culturax
@@ -15,6 +18,10 @@ def main():
         print(f"{output_file} not found. Starting download and processing...")
 
         load_data_from_culturax(model, N_SAMPLES, MIN_LEN, MAX_LEN, output_file, NORMALIZE_EMBEDDINGS)
+
+    resp_es = initialize_elsticsearch(output_file)
+    resp_qdrant = load_data_to_qdrant()
+    # to samo dla qdranta napisać :)
 
     answer = rag_query(USER_QUERY)
     print("Odpowiedź modelu to: ", answer)
